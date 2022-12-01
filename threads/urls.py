442887d7bd8ecde228c35e96b8eac26b293f77bc
jmgraham5 <83609009+jmgraham5@users.noreply.github.com>
2022-12-01 00:30:25 -0500

    working on basic styling

--- threads/urls.py
+++ threads/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.ListPosts.as_view(), name='home'),
     path('threads/new/', views.CreatePost.as_view(), name='new'),
     path('threads/<int:pk>/', views.ListPosts.as_view(), name='home'),
+    path('threads/<int:pk>/edit', views.EditPost.as_view(), name='edit'),
+    path('threads/<int:pk>/delete/', views.DeletePost.as_view(), name='delete'),
     path('home/register/', views.HomeRegister, name='home_register')
 ]
