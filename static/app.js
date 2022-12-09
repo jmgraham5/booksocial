f705150fe5946b9302dc79145a9ef88d9ac61328
jmgraham5 <83609009+jmgraham5@users.noreply.github.com>
2022-12-09 00:33:39 -0500

    'updated capstone project'

--- static/app.js
+++ static/app.js
@@ -4,17 +4,21 @@ const app = Vue.createApp({
         return {
             csrfToken: '',
             counter: 0
-        }
+        },
+        axios({
+            method: 'GET',
+            url: 'https://www.googleapis.com/books/v1',
+            headers: {
+                key : "AIzaSyD6Kae-WT53nONZryAncj-oJEUf5ZWy5ms"
+            }
+        }).then((response) => {
+            console.log(response.data)
+        })
     },
     methods: {
-        getList() {
-            this.axios.get(api).then((response) => {
-                console.log(response.data)
-            })
-        }
+        
     },  
     mounted(){
-       this.csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value,
-       console.log('hello')
+       this.csrfToken = document.querySelector("input[name=csrfmiddlewaretoken]").value
     }
 })
